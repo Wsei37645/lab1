b09cbbd0efdc550a87b52789b878a4aa9180bd1d
Wsei37645 <37645@studentwsei.pl>
2022-03-02 11:00:56 +0100

    Punkt 7b

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello Java World");
 
-        System.out.print("Ala");
-        System.out.print("ma");
-        System.out.print("kota");
+        System.out.println("Ala");
+        System.out.println("ma");
+        System.out.println("kota");
 
         
     }
