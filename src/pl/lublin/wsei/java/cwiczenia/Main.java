7f29d4ef98bebdecfbc9cbeb2fc03cadf7ed968c
Wsei37645 <37645@studentwsei.pl>
2022-03-02 11:32:59 +0100

    Punkt 7 g

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -15,6 +15,13 @@ public class Main {
         String s="jakiś tekst ";
 
         System.out.printf("a=%d, b=%.2f, s=%20s %n",a,b,s);
-        
+
+        System.out.print("Nazywaliśmy to \"witaminą B3\" \n");
+
+        System.out.printf("alfa\tsin(alfa) \n");
+        for (int i=0; i<370; i+=10)
+            System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+
+
     }
 }
