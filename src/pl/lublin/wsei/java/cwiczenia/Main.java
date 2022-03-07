42bf6d4f751cc56d89582a7a6ba474cb8819ecc3
Wsei37645 <37645@studentwsei.pl>
2022-03-07 14:52:01 +0100

    Punkt 9 bez błędu

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -36,16 +36,15 @@ public class Main {
         System.out.println("------------------------------------");
 
         Scanner input = new Scanner (System.in);
-        int num1, num2;
+        int num1=0, num2=0;
         do {
             System.out.print("Podaj pierwszą liczbę: ");
             num1 = input.nextInt();
-            if (num1 != 0){
-                System.out.print("Podaj drugą liczbę: ");
-                num2= input.nextInt();
-            }
+            System.out.print("Podaj drugą liczbę: ");
+            num2= input.nextInt();
+            if ((num1 ==  0) || (num2 == 0)) break;
             System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1+num2 ) ;
-        } while ((num1 !=  0) && (num2 != 0));
+        } while (true);
 
 
     }
