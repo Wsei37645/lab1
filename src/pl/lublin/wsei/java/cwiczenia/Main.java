d9c9acf50d4ad130fac593532d65f5ed6a439deb
Wsei37645 <37645@studentwsei.pl>
2022-03-07 14:18:21 +0100

    Punkt 7 h

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -3,25 +3,36 @@ package pl.lublin.wsei.java.cwiczenia;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("------------------------------------");
+        
 	    System.out.println("Hello Java World");
+        System.out.println("------------------------------------");
 
         System.out.print("Ala\n");
         System.out.print("ma\n");
         System.out.print("kota\n");
-
+        System.out.println("------------------------------------");
 
         int a=3;
         double b=4.21;
         String s="jakiś tekst ";
-
         System.out.printf("a=%d, b=%.2f, s=%20s %n",a,b,s);
+        System.out.println("------------------------------------");
 
         System.out.print("Nazywaliśmy to \"witaminą B3\" \n");
+        System.out.println("------------------------------------");
 
         System.out.printf("alfa\tsin(alfa) \n");
         for (int i=0; i<370; i+=10)
             System.out.printf("%d\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+        System.out.println("------------------------------------");
 
+        int x=0;
+        for (int arg=1; arg<9; arg++) {
+
+            System.out.printf( "%d\t%f\t%d\t\n",arg, Math.log(arg) / Math.log(2), x+=arg );
+        }
+        System.out.println("------------------------------------");
 
     }
 }
