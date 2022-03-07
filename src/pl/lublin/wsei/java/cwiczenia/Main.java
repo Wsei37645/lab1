8ee4d24ded3968356fc11a2ea6f40f5f840b32c5
Wsei37645 <37645@studentwsei.pl>
2022-03-07 14:36:26 +0100

    Punkt 7 h

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,5 +1,5 @@
 package pl.lublin.wsei.java.cwiczenia;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
@@ -35,5 +35,12 @@ public class Main {
         }
         System.out.println("------------------------------------");
 
+        Scanner input = new Scanner (System.in);
+        int num1, num2;
+        System.out.print("Podaj pierwszą liczbę: ");
+        num1 = input.nextInt();
+        System.out.print("Podaj drugą liczbę: ");
+        num2= input.nextInt();
+        System.out.printf("Wynik dodawania %d + %d = %d%n", num1, num2, num1+num2) ;
     }
 }
